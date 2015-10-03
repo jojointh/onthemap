f5330c0c6d4f16be91d1546681efc20806c6ecbf
JoJo <jojo141a@hotmail.com>
2015-10-04 04:51:38 +0700

    add map view

---
+++ On The Map/StudentMapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StudentMapViewController.swift
+//  On The Map
+//
+//  Created by Surasak Adulprasertsuk on 10/4/15.
+//  Copyright (c) 2015 Surasak. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class StudentMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
