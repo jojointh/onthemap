35d76a862dd59b67a170c2d14d4be162fd0468a1
JoJo <jojo141a@hotmail.com>
2015-10-04 22:42:41 +0700

    implement student list in table view

---
+++ On The Map/StudentTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StudentTableViewController.swift
+//  On The Map
+//
+//  Created by Surasak Adulprasertsuk on 10/4/15.
+//  Copyright (c) 2015 Surasak. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ParseClient.sharedInstance().studentInformationList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("StudentTableCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let student = ParseClient.sharedInstance().studentInformationList[indexPath.row]
+        
+        cell.textLabel?.text = "\(student.firstName) \(student.lastName)"
+        
+        return cell
+    }
+}
