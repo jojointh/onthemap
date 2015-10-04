c197bb6717aaa4d33eea008d3ea25dcd90e1073e
JoJo <jojo141a@hotmail.com>
2015-10-04 22:55:46 +0700

    plot student location pin to map

--- On The Map/StudentMapViewController.swift
+++ On The Map/StudentMapViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 import MapKit
 
-class StudentMapViewController: UIViewController {
+class StudentMapViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var annotations = [MKPointAnnotation]()
         let parameters = ["limit": 100, "order": "-updatedAt"]
         let task = ParseClient.sharedInstance().taskGetRequest(ParseClient.Methods.StudentLocation, parameters: parameters) { result, error in
             
@@ -32,8 +33,23 @@ class StudentMapViewController: UIViewController {
                     for student in result.valueForKey("results") as! [[String: AnyObject]] {
                         let studentInformation = StudentInformation(studentInfomation: student)
                         ParseClient.sharedInstance().studentInformationList.append(studentInformation)
+                        
+                        // create annotation point
+                        let lat = CLLocationDegrees(studentInformation.latitude)
+                        let long = CLLocationDegrees(studentInformation.longitude)
+                        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+                        let first = studentInformation.firstName
+                        let last = studentInformation.lastName
+                        let mediaURL = studentInformation.mediaURL
+                        var annotation = MKPointAnnotation()
+                        annotation.coordinate = coordinate
+                        annotation.title = "\(first) \(last)"
+                        annotation.subtitle = mediaURL
+                        annotations.append(annotation)
                     }
-                    //TODO: plot pin
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.mapView.addAnnotations(annotations)
+                    }
                 }
             }
         }
@@ -44,6 +60,38 @@ class StudentMapViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "Dismiss", style: .Default, handler: nil))
         dispatch_async(dispatch_get_main_queue()) {
             self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - MKMapViewDelegate
+    
+    // customize pin and annotation
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Red
+            pinView!.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIButton
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(mapView: MKMapView!, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == annotationView.rightCalloutAccessoryView {
+            let app = UIApplication.sharedApplication()
+            app.openURL(NSURL(string: annotationView.annotation.subtitle!)!)
         }
     }
 }
