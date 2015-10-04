ca2e94b8cb77c288e115e4c463c238eca36ceffa
JoJo <jojo141a@hotmail.com>
2015-10-05 00:52:33 +0700

    add study location find page

---
+++ On The Map/StudyLocationViewController.swift
@@ -0,0 +1,49 @@
+//
+//  StudyLocationViewController.swift
+//  On The Map
+//
+//  Created by Surasak Adulprasertsuk on 10/5/15.
+//  Copyright (c) 2015 Surasak. All rights reserved.
+//
+
+import UIKit
+
+class StudyLocationViewController: UIViewController, UITextViewDelegate{
+    
+    @IBOutlet weak var locationTextField: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func findLocation(sender: UIButton) {
+        if locationTextField.text.isEmpty {
+            displayAlert("Must Enter a Location.")
+        }
+    }
+    
+    @IBAction func dismiss(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    func displayAlert(message: String) {
+        let alert = UIAlertController(title: "", message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .Default, handler: nil))
+        dispatch_async(dispatch_get_main_queue()) {
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+}
